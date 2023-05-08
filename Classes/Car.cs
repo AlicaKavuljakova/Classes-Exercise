3cad64d897a978465d154403b6916ea3f8736dd3
alica kavuljakova <drobeek@gmail.com>
2023-05-08 09:54:32 -0700

    classes

---
+++ Classes/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Car
+    {
+        /*In the Car class:
+Create a Make property of type string that is public
+Create a Model property of type string that is public
+Create a Year property of type int that is public*/
+        public string Make {get; set;}
+        public string Model { get; set;}
+        public int Year { get; set;}
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* Create a new instance of the Car class
+ Set values in the properties for the object
+ Print the values of each property to the Console*/
+            var myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Year = 2013;
+            myCar.Model = "Corolla";
+            Console.WriteLine($"My car is {myCar.Make} {myCar.Model} made in {myCar.Year}");
         }
     }
 }
